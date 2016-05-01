5a902f8b70c1751767bb4272bccbce7881e963ab
Juan Camilo García <juancamilo87@outlook.com>
2016-05-02 00:05:09 +0300

    Test2 Comment

--- app/src/main/java/dev/kodama/test/MainActivity.java
+++ app/src/main/java/dev/kodama/test/MainActivity.java
@@ -24,7 +24,7 @@ import android.view.ViewGroup;
 import android.widget.PopupWindow;
 import android.widget.Toast;
 
-//Main Activity
+//Main Act2ivity3
 public class MainActivity extends AppCompatActivity
         /*implements Communicator*/ {
 
