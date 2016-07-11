e77d5c693861fc0bec6928f4312f9b2d8502a5d6
pabloag5 <pabloag5@gmail.com>
2016-07-11 18:13:54 -0500

    kda buttons theme kda buttons query method

--- app/src/main/java/dev/kodama/test/kdaFragment.java
+++ app/src/main/java/dev/kodama/test/kdaFragment.java
@@ -1,12 +1,15 @@
 package dev.kodama.test;
 
+import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -14,6 +17,9 @@ import java.util.List;
 
 
 public class kdaFragment extends Fragment {
+    Button btn1kda;
+    Button btn2kda;
+    Button btn3kda;
 
 
     @Override
@@ -22,9 +28,56 @@ public class kdaFragment extends Fragment {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.kdalayout, container, false);
 
-
+        btn1kda=(Button) view.findViewById(R.id.btn1kda);
+        btn2kda=(Button) view.findViewById(R.id.btn2kda);
+        btn3kda=(Button) view.findViewById(R.id.btn3kda);
+        btn1kda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn1kda.setSelected(true);
+                btn2kda.setSelected(false);
+                btn3kda.setSelected(false);
+                kdabtnqueries(v);
+            }
+        });
+        btn2kda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn1kda.setSelected(false);
+                btn2kda.setSelected(true);
+                btn3kda.setSelected(false);
+                kdabtnqueries(v);
+            }
+        });
+        btn3kda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn1kda.setSelected(false);
+                btn2kda.setSelected(false);
+                btn3kda.setSelected(true);
+                kdabtnqueries(v);
+            }
+        });
         return view;
 
+
+    }
+
+    private void kdabtnqueries(View btn) {
+        //games queries
+        switch (btn.getId()){
+            case R.id.btn1kda:
+                //query recent games
+                break;
+            case R.id.btn2kda:
+                //query weekly games
+                break;
+            case R.id.btn3kda:
+                //query season games
+                break;
+            default:
+                break;
+        }
 
     }
 
